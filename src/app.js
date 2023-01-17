307ad01971dd0cb36267afb0feff3e56cb26c641
Grünhut Gábor <Herman Lszl>
2023-01-17 19:08:26 +0100

    Fejrészek adása az Index.html és az app.js részére.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+/*
+File: index.html
+Author: Grünhut Gábor
+Copyright: 2023, Grünhut Gábor
+Group: Szoft I 1/E
+Date: 2023-01-17
+Github: https://github.com/GaborGrunhut/haromszog.git
+Licenc: GNU GPL
+*/
+
+
 const baseElem = document.querySelector("#base");
 const heightElem = document.querySelector("#height");
 const calcButton = document.querySelector("#calcButton");
